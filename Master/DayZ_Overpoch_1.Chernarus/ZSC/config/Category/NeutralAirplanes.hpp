25ec4390e8e46c5c39c5b0e77ec05eb9a37b31b9
Woot <war_greebo@hotmail.com>
2015-04-21 22:24:09 +0100

    Modified ZSC airplanes
    Included jets into config.

--- Master/DayZ_Overpoch_1.Chernarus/ZSC/config/Category/NeutralAirplanes.hpp
+++ Master/DayZ_Overpoch_1.Chernarus/ZSC/config/Category/NeutralAirplanes.hpp
@@ -39,4 +39,39 @@ class Category_517 {
 		buy[] ={40000,"Coins"};
 		sell[] ={20000,"Coins"};
 	};
+	class L39_TK_EP1 {
+		type = "trade_any_vehicle";
+		buy[] ={10000000,"Coins"};
+		sell[] ={5000000,"Coins"};
+	};
+	class Su25_TK_EP1 {
+		type = "trade_any_vehicle";
+		buy[] ={10000000,"Coins"};
+		sell[] ={5000000,"Coins"};
+	};
+	class Su39 {
+		type = "trade_any_vehicle";
+		buy[] ={10000000,"Coins"};
+		sell[] ={5000000,"Coins"};
+	};
+	class Su34 {
+		type = "trade_any_vehicle";
+		buy[] ={11000000,"Coins"};
+		sell[] ={5500000,"Coins"};
+	};
+	class AV8B {
+		type = "trade_any_vehicle";
+		buy[] ={12000000,"Coins"};
+		sell[] ={6000000,"Coins"};
+	};
+	class A10_US_EP1 {
+		type = "trade_any_vehicle";
+		buy[] ={13000000,"Coins"};
+		sell[] ={6500000,"Coins"};
+	};
+	class F35B {
+		type = "trade_any_vehicle";
+		buy[] ={14000000,"Coins"};
+		sell[] ={7000000,"Coins"};
+	};
 };
