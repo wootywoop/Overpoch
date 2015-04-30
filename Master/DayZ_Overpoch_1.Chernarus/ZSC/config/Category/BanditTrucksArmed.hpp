0eb8414b2aec80364421d4fce1f59b5c379f10bb
Woot <JSalkeld@PC1072Win10.msadl.fairport.co.uk>
2015-04-30 16:28:44 +0100

    Modified bandit and hero prices for trucks

--- Master/DayZ_Overpoch_1.Chernarus/ZSC/config/Category/BanditTrucksArmed.hpp
+++ Master/DayZ_Overpoch_1.Chernarus/ZSC/config/Category/BanditTrucksArmed.hpp
@@ -1,8 +1,8 @@
 class Category_534 {
 	class ArmoredSUV_PMC_DZE {
 		type = "trade_any_vehicle";
-		buy[] ={200000,"Coins"};
-		sell[] ={55000,"Coins"};
+		buy[] ={500000,"Coins"};
+		sell[] ={125000,"Coins"};
 	};
 	class Pickup_PK_TK_GUE_EP1_DZE {
 		type = "trade_any_vehicle";
@@ -16,17 +16,17 @@ class Category_534 {
 	};
 	class Offroad_SPG9_Gue {
 		type = "trade_any_vehicle";
-		buy[] ={500000,"Coins"};
-		sell[] ={115000,"Coins"};
+		buy[] ={150000,"Coins"};
+		sell[] ={37500,"Coins"};
 	};
 	class Pickup_PK_GUE_DZE {
 		type = "trade_any_vehicle";
-		buy[] ={75000,"Coins"};
-		sell[] ={27500,"Coins"};
+		buy[] ={100000,"Coins"};
+		sell[] ={25500,"Coins"};
 	};
 	class Pickup_PK_INS_DZE {
 		type = "trade_any_vehicle";
-		buy[] ={75000,"Coins"};
-		sell[] ={27500,"Coins"};
+		buy[] ={100000,"Coins"};
+		sell[] ={25500,"Coins"};
 	};
 };
