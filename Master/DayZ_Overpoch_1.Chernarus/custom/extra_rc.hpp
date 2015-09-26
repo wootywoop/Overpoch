4195603b7f248b865e514859bf800c4c934d138f
Woot <war_greebo@hotmail.com>
2015-09-26 15:59:40 +0100

    Fixed DZGM

--- Master/DayZ_Overpoch_1.Chernarus/custom/extra_rc.hpp
+++ Master/DayZ_Overpoch_1.Chernarus/custom/extra_rc.hpp
@@ -6,15 +6,13 @@ class ExtraRc {
 		};
 	};
 	class ItemRadio {
-        class switchOnOff {
-            text = "Switch ON/OFF";
-            script = "execVM 'custom\remote\switch_on_off.sqf'";
-        };
-    };
-	class ItemRadio {
      class GroupManagement {
         text = "Group Management";
         script = "execVM 'dzgm\loadGroupManagement.sqf'";
 		};
+	class switchOnOff {
+            text = "Switch ON/OFF";
+            script = "execVM 'custom\remote\switch_on_off.sqf'";
+        };
 	};
 };
